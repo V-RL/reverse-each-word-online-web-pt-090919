1689391cc09e7f6bbb63e5c03724e241b8a019d2
Brandon Furr <brandon.furr123@gmail.com>
2019-09-22 22:11:24 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  reverse_word = sentence.split(" ")
+  by_word = []
+  reverse_word.collect do |sentence|
+    by_word << sentence.reverse
+  end
+    by_word.join(" ")
+end
+  
