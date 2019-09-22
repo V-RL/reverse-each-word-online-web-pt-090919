4c3d81d74c7feb3807ccdb95cc67b6e3e5d610fc
Brandon Furr <brandon.furr123@gmail.com>
2019-09-22 21:40:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reverse_word = sentence.split 
-  reverse_word.each do |sentence|
+  reverse_word.collect do |sentence|
     sentence.reverse
     return "#{sentence}"
    
